25ddd145f23fb546653af67d2956410ed096aedc
k <mol.tired@gmail.com>
2021-04-23 16:39:34 -0500

    initialize messages bi-directional channel

--- mail.go
+++ mail.go
@@ -8,71 +8,54 @@ import (
 )
 
 type sender struct {
-	username    string
-	password    string
-	host        string
-	message     *gomail.Message
-	dialer      *gomail.Dialer
-	observers   []string
-	messages200 <-chan struct{}
-	messages500 <-chan string
+	username string
+	password string
+	to       string
+	host     string
+	port     int
+	message  *gomail.Message
+	dialer   *gomail.Dialer
+	messages chan string
 }
 
-func NewSender(host, port, username, password string) *sender {
+func NewSender(host string, port int, username, password, to string) *sender {
 	m := gomail.NewMessage()
 	d := gomail.NewDialer(host, port, username, password)
-	o := []string{username}
+	c := make(chan string)
 
 	m.SetHeader("From", username)
-	d.TLSConfig = &tls.Config{InsecureSkipVerify: false}
+	d.TLSConfig = &tls.Config{ServerName: host, InsecureSkipVerify: false}
 	return &sender{
 		username: username,
 		password: password,
 		host:     host,
+		port:     port,
+		to:       to,
+		messages: c,
 		message:  m,
 		dialer:   d,
-		observer: o,
 	}
 }
 
-func (s *sender) Messages200() {
-	s.messages200 <- struct{}{}
-}
-
-func (s *sender) Messages500(mssg500) {
-	s.messages500 <- mssg500
+func (s *sender) Messages(mssg string) {
+	s.messages <- mssg
 }
 
 func (s *sender) Dispatcher() {
 	for {
 		select {
-		case <-messages200:
-			s.notifyAll()
-		case mssg := <-messages500:
-			s.notifyError(mssg)
+		case mssg := <-s.messages:
+			s.notify(mssg)
 		}
 	}
 }
 
-func (s *sender) notifyError(mssg) {
-	log.Println("[!] something was wrong.")
-	s.message.SetHeader("Subject", "[!] something was wrong.")
+func (s *sender) notify(mssg string) {
+	log.Println("[!] sending an email.")
+	s.message.SetHeader("To", s.to)
+	s.message.SetHeader("Subject", mssg)
 	s.message.SetBody("text/plain", mssg)
-	s.message.SetHeader("To", s.observers[0])
-	if err := d.DialAndSend(s.message); err != nil {
+	if err := s.dialer.DialAndSend(s.message); err != nil {
 		log.Printf(err.Error())
-	}
-}
-
-func (s *sender) notifyAll() {
-	log.Println("[!] GPU available.")
-	s.message.SetHeader("Subject", "available.")
-	s.message.SetBody("text/plain", "available.")
-
-	for _, emailAddress := range s.observers {
-		s.message.SetHeader("To", emailAddress)
-		if err := d.DialAndSend(s.message); err != nil {
-			log.Printf(err.Error())
-		}
 	}
 }
