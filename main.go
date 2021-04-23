3517203a824b3c7de480257095b96fa3b399bd35
k <mol.tired@gmail.com>
2021-04-23 18:09:08 -0500

    fix typo flags documentacion

--- main.go
+++ main.go
@@ -10,9 +10,9 @@ func main() {
 
 	var (
 		apiURL       = flag.String("apiUrl", "", "Specify the url to request to.")
-		fromEmail    = flag.String("fromEmail", "", "Set the email messages sender.")
-		fromEmailKey = flag.String("fromEmailKey", "", "Set the email messages sender password.")
-		toEmail      = flag.String("toEmail", "", "Set the email messages receiver.")
+		fromEmail    = flag.String("fromEmail", "", "Set the email sender.")
+		fromEmailKey = flag.String("fromEmailKey", "", "Set the email sender password.")
+		toEmail      = flag.String("toEmail", "", "Set the email receiver.")
 		logFile      = flag.String("logFile", "out", "Specify a log file.")
 		smtpHost     = flag.String("smtpHost", "smtp.gmail.com", "Specify the smtp host name to send messages.")
 		smtpPort     = flag.Int("smtpPort", 587, "Specify the smtp port to send messages.")
